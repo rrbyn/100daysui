1f6da4171baa92f1eada58129e077be39dcb7013
matveimug <madis.vaher@gmail.com>
2020-12-07 14:04:15 +0200

    sass, router

---
+++ src/views/Home.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="home">
+    <h1>100 days of ui challenge</h1>
+    Siin on repo, kuhu panna enda UI-harjutused. Kasuta Vue routerit ja route, et ülesandeid eraldada, css võib olla inline.
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  name: 'Home',
+  components: {
+    HelloWorld
+  }
+}
+</script>
